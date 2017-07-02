983751b2b85fcfb06b7e83302a5c190cf8eeef71
mayesterday <programing@hotmail.co.uk>
2017-07-02 20:10:16 +0700

    ไำ

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$x = "ทดสอบการทำงาน";
+			$x = exec('pwd');
 
 			$messages = [
 				'type' => 'text',
