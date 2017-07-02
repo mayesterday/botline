5ea3558ff004d9bdc4f92aef6c504d0a1314f6fe
mayesterday <programing@hotmail.co.uk>
2017-07-02 20:54:39 +0700

    --

--- response.php
+++ response.php
@@ -1,26 +1,2 @@
 <?php
-$strAccessToken = '3tdnk8j59ClBebP8ZbrypNu12buF9sn5vdF8ATi4fCNXQYZ6/GbNM6UXNDeFHrFsqD9j4NkbDjy6CFYqmEAJ2t+a/rsWTMynx/vxqGoVS7pFEsedI89JcxDJWviBXeq5B8L6TZkd+bQ1LBpzHNrBjQdB04t89/1O/w1cDnyilFU=';
-
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-
-$x = json_encode(exec('ifconfig'));
-$arrPostData = array();
-$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-$arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "C $x";
-
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close($ch);
+echo "DF";
